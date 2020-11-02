a163da058663dbbc33ea2309e7a41215810cf111
Nolan Venkiah <venkiahn@amazon.com>
2020-11-02 15:25:45 +0200

    Manual Review

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Welcome World! This is a site by Nolan Venkiah.'));
+app.get('/', (req, res) => res.send('Welcome World! You have arrived at a site created by Nolan Venkiah.'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
